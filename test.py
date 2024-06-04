ebe0c744555eb32d4f70ca61c4e8dfd968a0b2a4
csknyy <55134627+csknyy@users.noreply.github.com>
2024-06-04 16:17:09 +1200

    Update test.py

--- test.py
+++ test.py
@@ -24,6 +24,8 @@ if req.status_code == requests.codes.ok:
     # req is now a dict with keys: name, encoding, url, size ...
     # and content. But it is encoded with base64.
     content = base64.b64decode(req['content'])
-    st.write(content)
+    jsonString = content.decode('utf-8')
+    #finalJson = json.loads(jsonString)
+    st.write(jsonString)
 else:
     st.write('Content was not found.')
