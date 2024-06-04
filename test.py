b06ac35c1543ba2416869bd8bc30d3a7a4f9eea4
csknyy <55134627+csknyy@users.noreply.github.com>
2024-06-04 16:13:34 +1200

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,28 @@
-import os
-import streamlit as st
-from github import Github
+#import os
+#import streamlit as st
+#from github import Github
 
 # Load GitHub access token from environment variable
 #token = os.environ["GITHUB_TOKEN"] = "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"
 
-import requests
+#import requests
 #url = 'https://github.com/csknyy/monster/blob/b0555c860210870ef7eff781899f6194fc1b4fec/update_this.txt'
-url = 'https://raw.githubusercontent.com/csknyy/monster/blob/b0555c860210870ef7eff781899f6194fc1b4fec/update_this.txt'
-page = requests.get(url)
-st.write(page.text)
+#page = requests.get(url)
+#st.write(page.text)
+
+import base64
+import requests
+
+user = 'csknyy'
+repo_name = 'monster'
+path_fo_file = 'update_this.txt'
+url = 'https://api.github.com/repos/{user}/{repo_name}/contents/{path_to_file}'
+req = requests.get(url)
+if req.status_code == requests.codes.ok:
+    req = req.json()  # the response is a JSON
+    # req is now a dict with keys: name, encoding, url, size ...
+    # and content. But it is encoded with base64.
+    content = base64.decodestring(req['content'])
+  st.write(content)
+else:
+    st.write('Content was not found.')
