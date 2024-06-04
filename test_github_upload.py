39ebef114b45aeed08a2ac11351bcc5155b9d804
csknyy <55134627+csknyy@users.noreply.github.com>
2024-06-05 10:23:27 +1200

    Update test_github_upload.py

--- test_github_upload.py
+++ test_github_upload.py
@@ -5,8 +5,8 @@ API_token = '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'
 username = 'csknyy@hotmail.com'
 password = 'At@han2014'
 
-#g = Github(username, password)
-g = Github(API_token)
+g = Github(login_or_token=username,password=password)
+#g = Github(API_token)
 repo = g.get_repo("csknyy/monster", lazy=False)
 #issues = repo.get_issues(state="open")
 
