1a1b58340826fc43f32130c78af2651564f3c8c9
csknyy <55134627+csknyy@users.noreply.github.com>
2024-06-05 09:36:41 +1200

    Update test_github_upload.py

--- test_github_upload.py
+++ test_github_upload.py
@@ -3,7 +3,7 @@ import os
 
 token = os.getenv('GITHUB_TOKEN', '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')
 g = Github(token)
-repo = g.get_repo("MartinHeinz/python-project-blueprint")
+repo = g.get_repo("csknyy/monster")
 issues = repo.get_issues(state="open")
 
 st.write(issues)
