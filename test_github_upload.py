05ab8fb861df222555944f0dcdb9f531e205940b
csknyy <55134627+csknyy@users.noreply.github.com>
2024-06-05 09:43:30 +1200

    Update test_github_upload.py

--- test_github_upload.py
+++ test_github_upload.py
@@ -1,7 +1,9 @@
+API_token = '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'
+
 from github import Github
 import os
 
-token = os.getenv('GITHUB_TOKEN', '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')
+token = os.getenv('GITHUB_TOKEN', API_token)
 g = Github(token)
 repo = g.get_repo("csknyy/monster")
 issues = repo.get_issues(state="open")
