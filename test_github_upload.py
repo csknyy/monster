97c63afeeafb31f84d7e5c92a0a3831ed58171ba
csknyy <55134627+csknyy@users.noreply.github.com>
2024-06-05 10:27:56 +1200

    Update test_github_upload.py

--- test_github_upload.py
+++ test_github_upload.py
@@ -1,34 +1,22 @@
 from github import Github
 import os
 
-API_token = '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'
-username = 'csknyy@hotmail.com'
-password = 'At@han2014'
+import base64
+import requests
+import streamlit as st
 
-g = Github(login_or_token=username,password=password)
-#g = Github(API_token)
-repo = g.get_repo("csknyy/monster", lazy=False)
-#issues = repo.get_issues(state="open")
-
-rate_limit = g.get_rate_limit()
-core_rate_limit = rate_limit.core
-
-if core_rate_limit.remaining == 0:
-    st.error(f"Rate limit exceeded. Reset at {core_rate_limit.reset}")
+user = 'csknyy'
+repo_name = 'monster'
+path_to_file = 'update_this.csv'
+url = f'https://api.github.com/repos/{user}/{repo_name}/contents/{path_to_file}'
+req = requests.get(url)
+if req.status_code == requests.codes.ok:
+    req = req.json()  # the response is a JSON
+    # req is now a dict with keys: name, encoding, url, size ...
+    # and content. But it is encoded with base64.
+    content = base64.b64decode(req['content'])
+    jsonString = content.decode('utf-8')
+    #finalJson = json.loads(jsonString)
+    st.write(jsonString)
 else:
-    # Display a text input box and a button in the Streamlit app
-    st.title("GitHub File Updater")
-    input_text = st.text_area("Enter text to update the file")
-    update_button = st.button("Update File")
-
-    # Handle button click event
-    if update_button:
-        # Get the current file content
-        file = repo.get_contents(path_to_file)
-        current_content = base64.b64decode(file.content).decode('utf-8')
-
-        # Update the file content
-        new_content = current_content + '\n' + input_text
-        repo.update_file(path_to_file, 'Updated file', new_content, file.sha)
-
-        st.success("File updated successfully!")
+    st.write('Content was not found.')
