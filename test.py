60cfd7ccd9086d03a164c25ed3df8af20ef9d474
csknyy <55134627+csknyy@users.noreply.github.com>
2024-06-04 16:10:45 +1200

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from github import Github
 #token = os.environ["GITHUB_TOKEN"] = "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"
 
 import requests
-url = 'https://github.com/csknyy/monster/blob/b0555c860210870ef7eff781899f6194fc1b4fec/update_this.txt'
+#url = 'https://github.com/csknyy/monster/blob/b0555c860210870ef7eff781899f6194fc1b4fec/update_this.txt'
+url = 'https://raw.githubusercontent.com/csknyy/monster/blob/b0555c860210870ef7eff781899f6194fc1b4fec/update_this.txt'
 page = requests.get(url)
 st.write(page.text)
