6ed7b02d96aa09e4f362fe122d6fad524d139a45
csknyy <55134627+csknyy@users.noreply.github.com>
2024-06-04 15:44:41 +1200

    Update test.py

--- test.py
+++ test.py
@@ -1,73 +1,19 @@
-#####
-#I9AZTHT82B4K6NURHWEIB635UHX4WS9S
-#####
-
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-
-# Path to your text file
-file_path = 'test.txt'
-
-# Function to read the file
-def read_file():
-    with open(file_path, 'r') as file:
-        content = file.read()
-    return content
-
-# Function to write to the file
-def write_file(new_content):
-    with open(file_path, 'a') as file:
-        file.write('\n' + new_content)
-
-@app.route('/read', methods=['GET'])
-def get_content():
-    content = read_file()
-    return jsonify({"content": content})
-
-@app.route('/write', methods=['POST'])
-def add_content():
-    new_content = request.json.get('new_content')
-    if new_content:
-        write_file(new_content)
-        return jsonify({"message": "New lines added successfully"}), 200
-    else:
-        return jsonify({"error": "No content provided"}), 400
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
 import streamlit as st
-import requests
+from github import Github
+from access_token import token
 
-# Define the URL of the Flask server endpoints
-read_url = "https://coskunyay.com/read"
-write_url = "https://coskunyay.com/write"
+token = "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"
+account = Github(token)
 
-# Fetch the file contents from the Flask server
-response = requests.get(read_url)
+organization = account.get_organization("csknyy")
 
-# Check if the request was successful
-if response.status_code == 200:
-    file_content = response.json()["content"]
-else:
-    file_content = "Failed to fetch the file."
+repo = organization.get_repo("monster")
 
-# Display the current content in Streamlit
-st.title("Text File Content")
-st.text_area("Current File Content", value=file_content, height=300, disabled=True)
+file_path = "update_this.txt"
 
-# Add a text area for adding new content
-new_content = st.text_area("Add New Lines")
+text_input = st.textbox()
 
-# Add a button to save the new content
-if st.button("Save Changes"):
-    # Send the new content to the Flask server
-    response = requests.post(write_url, json={"new_content": new_content})
+update_button = st.button()
 
-    # Check if the update request was successful
-    if response.status_code == 200:
-        st.success("New lines added successfully!")
-    else:
-        st.error("Failed to add new lines.")
+if update_button == True:
+    repo.update_file(path=file_path)
