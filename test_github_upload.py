a18847ba1ba3bb174223905442b2b3615c5a9d33
csknyy <55134627+csknyy@users.noreply.github.com>
2024-06-05 10:10:49 +1200

    Update test_github_upload.py

--- test_github_upload.py
+++ test_github_upload.py
@@ -1,10 +1,12 @@
 API_token = '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'
+username = 'csknyy@hotmail.com'
+password = 'At@han2014'
 
 from github import Github
 import os
 
-g = Github(API_token)
-repo = g.get_repo("csknyy/monster")
-issues = repo.get_issues(state="open")
+g = Github(username, password)
+repo = g.get_repo("csknyy/monster", lazy=False)
+#issues = repo.get_issues(state="open")
 
-st.write(issues)
+st.write(repo)
