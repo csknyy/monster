95686392b2dd275f48da95ae5993888dc11e16fd
csknyy <55134627+csknyy@users.noreply.github.com>
2024-06-05 10:28:46 +1200

    Update test_github_upload.py

--- test_github_upload.py
+++ test_github_upload.py
@@ -10,13 +10,13 @@ repo_name = 'monster'
 path_to_file = 'update_this.csv'
 url = f'https://api.github.com/repos/{user}/{repo_name}/contents/{path_to_file}'
 req = requests.get(url)
-if req.status_code == requests.codes.ok:
-    req = req.json()  # the response is a JSON
-    # req is now a dict with keys: name, encoding, url, size ...
-    # and content. But it is encoded with base64.
-    content = base64.b64decode(req['content'])
-    jsonString = content.decode('utf-8')
-    #finalJson = json.loads(jsonString)
-    st.write(jsonString)
-else:
-    st.write('Content was not found.')
+#if req.status_code == requests.codes.ok:
+req = req.json()  # the response is a JSON
+# req is now a dict with keys: name, encoding, url, size ...
+# and content. But it is encoded with base64.
+content = base64.b64decode(req['content'])
+jsonString = content.decode('utf-8')
+#finalJson = json.loads(jsonString)
+st.write(jsonString)
+#else:
+#    st.write('Content was not found.')
